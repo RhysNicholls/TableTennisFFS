8ee208af4f5322ede2888e60f7db028a7c941ebf
shiftyllama <shiftyllama@googlemail.com>
2016-02-22 17:08:21 +0000

    Updated +Team +Set +Reg Manager

--- src/tabletennis201516/RegistrationManager.java
+++ src/tabletennis201516/RegistrationManager.java
@@ -6,10 +6,52 @@
 
 package tabletennis201516;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jin
  */
 public class RegistrationManager {
-    
+
+
+    ArrayList<Player> players = new ArrayList<>();
+    ArrayList<Team> teams = new ArrayList<>();
+
+    public void addTeam(Team t) {
+        teams.add(t);
+    }
+
+    public Team getTeam(String teamName) {
+        int index = teams.indexOf(new Team(teamName));
+
+        if (index < 0) {
+            return null;
+        } else {
+            return teams.get(index);
+        }
+    }
+
+    public void regPlayerToTeam(Player p, Team t) {
+        for (Team ti : teams) {
+            if (ti.getName().equals(t.getName())) {
+                ti.addMember(p);
+                break;
+            }
+        }
+        players.add(p);
+
+    }
+
+    public ArrayList<Team> getAllTeams() {
+        return teams;
+    }
+
+    public ArrayList<Player> getAllPlayers() {
+        return players;
+    }
+
+    public boolean isTeamValid(Team t) {
+        return teams.contains(t);
+    }
 }
--- src/tabletennis201516/Team.java
+++ src/tabletennis201516/Team.java
@@ -5,19 +5,52 @@
  */
 package tabletennis201516;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jin
  */
 public class Team {
     private String tname;
+    private ArrayList<Player> players = new ArrayList<>();
     
-    public Team (String name){
-        
+   public Team(String tname) {
+        this.tname = tname;
+   }
+   
+   public void addMember(Player player) {
+        players.add(player);
+   }
+   
+   public boolean isPlayerValid(String playerName) {
+        return players.contains(new Player(playerName));
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
     }
     
-//    public void addTeamMember (Player){
-//        
-//    }
+      public Player getPlayer(String playerName) {
+        int i = players.indexOf(new Player(playerName));
+        if (i > -1) {
+            return players.get(i);
+        }
+        return null;
+
+    }
+      
+      public String getName() {
+        return tname;
+    }
+      
+     @Override
+    public String toString() {
+        return "Team{" + "name=" + tname;
+    }
     
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
 }
+
